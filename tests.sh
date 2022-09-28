0201367c16c74b0fe69edbb3fd690c0d2dac95bd
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2022-09-28 07:58:57 +0200

    WIP make tests run on bash

--- tests.sh
+++ tests.sh
@@ -10,19 +10,43 @@ trap 'summary; exit $?' EXIT
 tests=0
 failures=0
 
+echo() { printf '%s' "$@"; }
+hex() { xxd -p; }
+unhex() { xxd -p -r; }
+
+_roundtrip() {
+	od ${endianness:+"--endian=$endianness"} | ./rod ${endianness:+"--endian=$endianness"}
+}
+
 _test() {
+	__test cat hex "$1" "$2"
+}
+
+_test_hex() {
+	__test unhex cat "$1" "$2"
+}
+
+_test_stdin() {
+	_test_hex "$(hex)" "$1"
+}
+
+__test() {
 	tests=$((tests+1))
-	input="$1"
-	desc="${2:-"$1"}"
-	actual="$(printf '%s' "$input" | od ${endianness:+"--endian=$endianness"} | ./rod ${endianness:+"--endian=$endianness"} | xxd -p)"
-	expected="$(printf '%s' "$input" | xxd -p)"
-	if test "$actual" = "$expected"; then
+	unhex="$1"
+	hex="$2"
+	input="$3"
+	desc="${4:-$input}"
+	actual="$(echo "$input" | $unhex | _roundtrip | hex)"
+	expected="$(echo "$input" | $hex)"
+	if _eq "$actual" "$expected"; then
 		printf 'OK   %s\n' "$desc"
 	else
 		printf 'FAIL %s -- "%s" != "%s"\n' "$desc" "$actual" "$expected"
 		failures=$((failures+1))
 	fi
 }
+
+_eq() { test "$1" = "$2"; }
 
 for endianness in '' big little; do
 	printf 'Endian: %s\n' "${endianness:-default}"
@@ -39,10 +63,10 @@ for endianness in '' big little; do
 	_test '0123456789abcdef0123456789abcdef' 'repeated'
 	_test '0123456789abcdef0123456789abcdefxxxxxxxxxxxxxxxxfedcba9876543210fedcba9876543210fedcba9876543210fedcba9876543210//' 'repeated, normal, repeated'
 
-	_test "$(printf 000102030400 | xxd -r -p)" 'binary'
-	_test "$(printf 000000000000 | xxd -r -p)" 'nul bytes'
+	_test_hex '000102030400' 'binary'
+	_test_hex '000000000000' 'nul bytes'
 
 	_test "$(head -c100 /dev/urandom | base64)" 'base64'
-	_test "$(dd if=/dev/urandom bs=1M count=17)" 'large offsets'
-	_test "$(dd if=/dev/zero bs=1M count=17)" 'large repeat'
+	dd if=/dev/urandom bs=1M count=17 | _test_stdin 'large offsets'
+	dd if=/dev/zero bs=1M count=17 | _test_stdin 'large repeat'
 done
